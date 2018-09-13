1acc7c8ace7ab06db32a3f3d7351c164ba6bac63
a4goku <wangyi_062500@hotmail.com>
2018-09-13 09:20:34 +0800

    Initial commit

---
+++ src/main/java/com/jdmail/mailproductor/MailProductorApplication.java
@@ -0,0 +1,12 @@
+package com.jdmail.mailproductor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailProductorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MailProductorApplication.class, args);
+    }
+}
