cda609649e817511c205d036b2dde15e87251e2d
a4goku <wangyi_062500@hotmail.com>
2018-09-20 14:25:26 +0800

    添加一个redis测试

---
+++ src/main/java/com/jdmail/mail/utils/TestRedis.java
@@ -0,0 +1,12 @@
+package com.jdmail.mail.utils;
+
+import redis.clients.jedis.Jedis;
+
+public class TestRedis {
+    public static void main(String[] args){
+        Jedis jedis = new Jedis("127.0.0.1", 6379);
+        System.out.println("Server is running...:" + jedis.ping());
+        jedis.set("jedisTest", "jedisTest");
+        System.out.println(jedis.get("jedisTest"));
+    }
+}
